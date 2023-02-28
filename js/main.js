a07bc39cd9c6eee85646f02599d13929ffe35441
Cameron <cmartens@unca.edu>
2023-02-27 16:01:42 -0800

    Actually removed utilities.js this time

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-import {getAccessToken} from './utilities.js';
+//import {getAccessToken} from './utilities.js';
 const rootURL = 'https://photo-app-secured.herokuapp.com';
 const username = "cameron";
 const password = "cameron_password";
+
+async function getAccessToken(rootURL, username, password) {
+    const postData = {
+        "username": username,
+        "password": password
+    };
+    const endpoint = `${rootURL}/api/token/`;
+    const response = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData)
+    });
+    const data = await response.json();
+    return data.access_token;
+}
 
 const showStories = async (token) => {
     const endpoint = `${rootURL}/api/stories`;
